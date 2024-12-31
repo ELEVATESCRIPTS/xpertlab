0e9c1dcde9ecbe548ef746978eef326716c935b7
ELEVATE Solutions Limited <nurhossain.elevate@outlook.com>
2024-12-31 18:18:05 +0600

    Update connect.inc.php

--- includes/connect.inc.php
+++ includes/connect.inc.php
@@ -1,8 +1,15 @@
-<?php
+$host       = "xpertlabdb.mysql.database.azure.com";
+$username   = "pdbadm";
+$password   = "Asdf123456789";
+$db_name     = "blog";
+$sslcert    = "./DigiCertGlobalRootCA.crt.pem";
+
+  $conn = mysqli_init();
+        mysqli_ssl_set($conn,NULL,NULL,$sslcert,NULL,NULL);
+        if(!mysqli_real_connect($conn, $host, $username, $password, $db_name, 3306, MYSQLI_CLIENT_SSL)){
+                die('Failed to connect to MySQL: '.mysqli_connect_error());
+
+        }
 
 
-$host       = getenv('xpertlabdb.mysql.database.azure.com');
-$username   = getenv('pdbadm');
-$password   = getenv('Asdf123456789');
-$db_name     = getenv('blog');
-$sslcert    = "ssl/DigiCertGlobalRootCA.crt.pem";
+?>
