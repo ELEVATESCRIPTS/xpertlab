1c21567dc30a0453ff7d9e7f5370268cf3f6e312
ELEVATE Solutions Limited <nurhossain.elevate@outlook.com>
2024-12-31 19:02:31 +0600

    Update connect.inc.php

--- includes/connect.inc.php
+++ includes/connect.inc.php
@@ -1,5 +1,6 @@
 <?php
 
+<<<<<<< HEAD
 $host       = "xpertlabdb.mysql.database.azure.com";
 $username   = "pdbadm";
 $password   = "Asdf123456789";
@@ -10,5 +11,11 @@ $conn = new mysqli($host, $username, $password, $db_name, 3306, MYSQLI_CLIENT_SS
 if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
 }
+=======
+>>>>>>> parent of 0e9c1dc (Update connect.inc.php)
 
-?>
+$host       = getenv('xpertlabdb.mysql.database.azure.com');
+$username   = getenv('pdbadm');
+$password   = getenv('Asdf123456789');
+$db_name     = getenv('blog');
+$sslcert    = "ssl/DigiCertGlobalRootCA.crt.pem";
