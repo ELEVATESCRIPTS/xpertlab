6d6bf172c7fd338c4fff4a5e99e4b06cd9bcf6c7
ELEVATE Solutions Limited <nurhossain.elevate@outlook.com>
2024-12-31 10:26:08 +0600

    Update connect.inc.php

--- includes/connect.inc.php
+++ includes/connect.inc.php
@@ -1,6 +1,6 @@
 <?php
 $con = mysqli_init();
-mysqli_ssl_set($con,NULL,NULL, "{path to CA cert}", NULL, NULL);
+mysqli_ssl_set($con,NULL,NULL, "./DigiCertGlobalRootCA.crt.pem", NULL, NULL);
 mysqli_real_connect($conn, "xpertlabdb.mysql.database.azure.com", "dbadm", "dB@dm!n1243.", "blog", 3306, MYSQLI_CLIENT_SSL);
     if(!$con){
         header("Location: ../index.php?ERROR=connection_error");
