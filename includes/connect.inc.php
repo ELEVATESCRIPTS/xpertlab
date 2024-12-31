7198200d0002dd27ad0bec46c1e0578f99eee582
ELEVATE Solutions Limited <nurhossain.elevate@outlook.com>
2024-12-31 18:51:28 +0600

    change config

--- includes/connect.inc.php
+++ includes/connect.inc.php
@@ -6,12 +6,10 @@ $username   = "pdbadm";
 $password   = "Asdf123456789";
 $db_name     = "blog";
 $sslcert    = "./DigiCertGlobalRootCA.crt.pem";
-
-  $conn = mysqli_init();
-        mysqli_ssl_set($conn,NULL,NULL,$sslcert,NULL,NULL);
-        if(!mysqli_real_connect($conn, $host, $username, $password, $db_name, 3306, MYSQLI_CLIENT_SSL)){
-                die('Failed to connect to MySQL: '.mysqli_connect_error());
-
-        }
+$conn = new mysqli($host, $username, $password, $db_name, 3306, MYSQLI_CLIENT_SSL);
+// Check connection
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}
 
         ?>
